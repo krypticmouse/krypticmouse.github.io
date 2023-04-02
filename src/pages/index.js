2c9d19be60ba4389ea4ff7d462347f389e71c673
“krypticmouse” <herumbshandilya123@gmail.com>
2023-04-02 19:04:48 +0530

    init app

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Herumb Shandilya | krypticmouse</title>
+        <meta name="description" content="Web Portfolio & Research Journal of Herumb Shandilya." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/Krypticmouse.jpeg" />
+      </Head>
+      <main>
+        Hello
+      </main>
+    </>
+  )
+}
